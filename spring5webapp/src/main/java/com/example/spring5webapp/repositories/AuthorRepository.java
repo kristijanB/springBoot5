226726cab5f79064b92cfa06af9ea3bdc06ff4f9
kristijanB <kbrechel@foi.hr>
2019-08-01 08:13:52 +0200

    1

--- spring5webapp/src/main/java/com/example/spring5webapp/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring5webapp.repositories;
-
-import com.example.spring5webapp.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- spring5webapp/src/main/java/com/example/spring5webapp/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring5webapp.repositories;
-
-import com.example.spring5webapp.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
