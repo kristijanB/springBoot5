e8ba72f85f1a90f142ae024016caa03a30b0d31c
kristijanB <kbrechel@foi.hr>
2019-08-07 12:14:24 +0200

    rješenje

---
+++ spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/GreetingServiceFactory.java
@@ -0,0 +1,21 @@
+package guru.springframework.services;
+
+public class GreetingServiceFactory {
+    private GreetingRepository greetingRepository;
+
+    public GreetingServiceFactory(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public GreetingService createGreetingService(String lang){
+        switch (lang){
+            case "de":
+                return new PrimaryGermanGreetingService(greetingRepository);
+            case "es":
+                return new PrimarySpanishGreetingService(greetingRepository);
+            case "en":
+            default:
+                return new PrimaryGreetingService(greetingRepository);
+        }
+    }
+}
