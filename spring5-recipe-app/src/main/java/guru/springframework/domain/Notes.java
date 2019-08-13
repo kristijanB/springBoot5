13377d2ab25c9300c1dd554c8d84dbaa551ece0c
kristijanB <kbrechel@foi.hr>
2019-08-13 13:35:00 +0200

    Adding project lombok Closes #51

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Notes.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Notes.java
@@ -1,7 +1,13 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
@@ -12,32 +18,8 @@ public class Notes {
     @OneToOne
     private Recipe recipe;
 
-
     //za velike objekte 255 znakova(default), klob(za string) ili blob u binarnom obliku(slike..)
     @Lob
     private String recipeNotes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
