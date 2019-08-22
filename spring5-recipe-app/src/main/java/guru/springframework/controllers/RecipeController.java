f9eb146efe6f8b2055bba0ac826b63cfdefca9de
kristijanB <kbrechel@foi.hr>
2019-08-22 10:33:01 +0200

    lekcija 204

--- spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -43,6 +43,6 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show/";
     }
 }
