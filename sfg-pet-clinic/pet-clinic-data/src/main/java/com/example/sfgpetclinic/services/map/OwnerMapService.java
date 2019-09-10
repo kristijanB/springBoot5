a57cc367314bcca1fa1f4937509d7f7701dddf31
kristijanB <kbrechel@foi.hr>
2019-09-10 12:13:28 +0200

    imol find owner. Closes #54

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/OwnerMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.example.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -67,4 +68,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     public Owner findByLastName(String lastName) {
         return null;
     }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return null;
+    }
+
+
 }
