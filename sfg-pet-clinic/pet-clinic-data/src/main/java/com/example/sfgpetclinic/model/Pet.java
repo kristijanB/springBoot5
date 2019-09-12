e05f7c3215e33633ecf0173861f2e9b0a5a4141c
kristijanB <kbrechel@foi.hr>
2019-09-12 10:49:58 +0200

    added visit controller and mappings. Closes #57

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -11,26 +11,37 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
-
+    
+    @Builder
+    public Pet(Long id, PetType petType, Owner owner, LocalDate birthDate, String name, Set<Visit> visits) {
+        super(id);
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.name = name;
+        if (visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
+    }
+    
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
-
+    
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-
+    
     @Column(name = "birth_date")
     private LocalDate birthDate;
-
+    
     @Column(name = "name")
     private String name;
-
+    
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
+    
 }
