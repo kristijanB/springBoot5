3ce1f9b876d47e792e9480e14d94e54bcf53cb7a
kristijanB <kbrechel@foi.hr>
2019-09-23 15:29:15 +0200

    251 lekcija-popraviti učitavanje null vrijednosti kod veterinara

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -9,109 +9,110 @@ import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
-
+    
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
     private final VisitService visitService;
-
+    
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
-
+        
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
         this.visitService = visitService;
     }
-
+    
     @Override
     public void run(String... args) throws Exception {
-
+        
         int count = petTypeService.findAll().size();
         if (count == 0) {
-
+            
             loadData();
-
+            
         }
-
+        
     }
-
+    
     private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType saveDogType = petTypeService.save(dog);
-
+        
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType saveCatType = petTypeService.save(cat);
-
+        
         Speciality radiology = new Speciality();
         radiology.setDescription("Radiology");
         Speciality savedRadiology = specialtyService.save(radiology);
-
+        
         Speciality surgery = new Speciality();
         radiology.setDescription("Surgery");
         Speciality savedSurgery = specialtyService.save(surgery);
-
+        
         Speciality dentistry = new Speciality();
         radiology.setDescription("Dentistry");
         Speciality savedDentistry = specialtyService.save(dentistry);
-
+        
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         owner1.setAddress("123 Adresa");
         owner1.setCity("Zagreb");
         owner1.setTelephone("123123");
-
+        
         Pet mikesPet = new Pet();
         mikesPet.setPetType(saveDogType);
         mikesPet.setOwner(owner1);
         mikesPet.setBirthDate(LocalDate.now());
         mikesPet.setName("Rosco");
         owner1.getPets().add(mikesPet);
-
+        
         ownerService.save(owner1);
-
+        
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         owner2.setAddress("345 Adresa");
         owner2.setCity("Split");
         owner2.setTelephone("098098");
-
+        
         Pet fionasPet = new Pet();
         fionasPet.setName("Just cat");
         fionasPet.setOwner(owner2);
         fionasPet.setBirthDate(LocalDate.now());
         fionasPet.setPetType(saveCatType);
         owner2.getPets().add(fionasPet);
-
+        
         ownerService.save(owner2);
-
+        
         Visit catVisit = new Visit();
         catVisit.setPet(fionasPet);
         catVisit.setDate(LocalDate.now());
         catVisit.setDescription("Sneezy Kitty");
-
+        
         visitService.save(catVisit);
-
+        
         System.out.println("Loaded owners...");
-
+        
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vet1.getSpecialities().add(savedRadiology);
-
+        
         vetService.save(vet1);
-
+        
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         vet2.getSpecialities().add(savedSurgery);
+        
         vetService.save(vet2);
-
+        
         System.out.println("Load Vets...");
     }
 }
